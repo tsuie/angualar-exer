fcf7305655c412155055467e4169c60e9bc7a01e
unknown <alvinucsk@gmail.com>
2022-01-11 13:32:04 +0800

    - Creating Login components     - Created Login UI     - Adding neccessary classes for Login component     - Added field validation     - Login Error handling     - Login: Added redirection on authenticated user

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/user.service';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  userform = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]]
+  });
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private _userService: UserService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
   ngOnInit(): void {
+    console.log(this._userService.isLoggedIn());
+    if(this._userService.isLoggedIn()) {
+      this.document.location.href = '/dashboard';
+    }
+  }
+  onSubmit() {
+    console.log(this.userform.value);
+    console.log('Submit');
   }
 
 }
